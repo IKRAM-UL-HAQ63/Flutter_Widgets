fb2ccbdb863ee3a2e79923f5c58daf81259d772b
IKRAM-UL-HAQ63 <gk0336363@gmail.com>
2025-08-11 02:05:03 +0800

    change in container dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:new_project/firebase_file/auth_email_pass.dart';
 import 'package:new_project/firebase_options.dart';
-
+import 'package:new_project/widgets/textbtn_elevatedbtn.dart';
+import 'package:new_project/widgets/container.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Dropdown Example',
-      home: Day24Authentication(),
+      home: Profile_card(),
     );
   }
 }
--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -1,66 +1,129 @@
 import 'package:flutter/material.dart';
 
-class Profile_card extends StatelessWidget{
+class Profile_card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 300,
-          height: 150,
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(left: 20,top: 20),
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors:[Colors.red,Colors.lime,Colors.deepPurple]),
-              border:Border.all(color: Colors.black,width: 3),
-              borderRadius: BorderRadius.circular(15)
-          ),
-          child: Text("👤",style: TextStyle(fontSize: 30),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        title: Text(
+          "Container",
+          style: TextStyle(color: Colors.white, fontSize: 30),
         ),
-        Container(
-          width: 300,
-          height: 100,
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(left: 20,top: 10),
-          decoration: BoxDecoration(
-              gradient: SweepGradient(colors: [Colors.red,Colors.lime,Colors.deepPurple,Colors.lime,Colors.cyan,Colors.pinkAccent]),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 300,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(left: 20, top: 20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.red, Colors.lime, Colors.deepPurple],
+              ),
+              border: Border.all(color: Colors.black, width: 3),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Text("👤", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 20, top: 10),
+            decoration: BoxDecoration(
+              gradient: SweepGradient(
+                colors: [
+                  Colors.red,
+                  Colors.lime,
+                  Colors.deepPurple,
+                  Colors.lime,
+                  Colors.cyan,
+                  Colors.pinkAccent,
+                ],
+              ),
               borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.black,width: 3)
-          ),
-          child: Container(
+              border: Border.all(color: Colors.black, width: 3),
+            ),
+            child: Container(
               padding: EdgeInsets.only(left: 5),
-              child: Text("Name: IKRAM-RABIA",style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),)),
-        ),
-        Container(
-          width: 300,
-          height: 100,
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(left: 20,top: 10),
-          decoration: BoxDecoration(
-              gradient: SweepGradient(colors: [Colors.red,Colors.lime,Colors.deepPurple,Colors.lime,Colors.cyan,Colors.pinkAccent]),
+              child: Text(
+                "Name: IKRAM-RABIA",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 20, top: 10),
+            decoration: BoxDecoration(
+              gradient: SweepGradient(
+                colors: [
+                  Colors.red,
+                  Colors.lime,
+                  Colors.deepPurple,
+                  Colors.lime,
+                  Colors.cyan,
+                  Colors.pinkAccent,
+                ],
+              ),
               borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.black,width: 3)
-          ),
-          child: Container(
+              border: Border.all(color: Colors.black, width: 3),
+            ),
+            child: Container(
               padding: EdgeInsets.only(left: 5),
-              child: Text("Email: ikrab@email.com",style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),)),
-        ),
-        Container(
-          width: 300,
-          height: 100,
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(left: 20,top: 10),
-          decoration: BoxDecoration(
-              gradient: SweepGradient(colors: [Colors.red,Colors.lime,Colors.deepPurple,Colors.lime,Colors.cyan,Colors.pinkAccent]),
+              child: Text(
+                "Email: ikrab@email.com",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 20, top: 10),
+            decoration: BoxDecoration(
+              gradient: SweepGradient(
+                colors: [
+                  Colors.red,
+                  Colors.lime,
+                  Colors.deepPurple,
+                  Colors.lime,
+                  Colors.cyan,
+                  Colors.pinkAccent,
+                ],
+              ),
               borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.black,width: 3)
-          ),
-          child: Container(
+              border: Border.all(color: Colors.black, width: 3),
+            ),
+            child: Container(
               padding: EdgeInsets.only(left: 5),
-              child: Text("Country: Pakistan",style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),)),
-        ),
-      ],
+              child: Text(
+                "Country: Pakistan",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
-
   }
 }
